f4dfcda613fc02a389bbf58215bc62b47a1d8fd4
crakdelpol <matteo.pipitone93@gmail.com>
2018-01-18 01:02:27 +0100

    Implemented Kart

--- app.js
+++ app.js
@@ -7,11 +7,91 @@ myApp.config(function($routeProvider){
             controller: "BookListCtrl"
         })
         .when('/kart', {
-            templateUrl: "partials/kart-list.html"
+            templateUrl: "partials/kart-list.html",
+            controller: "KartListCtrl"
         })
         .otherwise({
             redirectTo: "/books"
         })
+});
+
+myApp.factory("bookService", function(){
+    $scope.books = 
+    [
+        {
+            name: "name1",
+            price: 1,
+            rating: 5,
+            releaseDate: "12/08/2017",
+            description: "BlaBlaBla"
+        },
+        {
+            name: "name2",
+            price: 2,
+            rating: 5,
+            releaseDate: "12/08/2017",
+            description: "BlaBlaBla"
+        },
+        {
+            name: "name3",
+            price: 3,
+            rating: 5,
+            releaseDate: "12/08/2017",
+            description: "BlaBlaBla"
+        },
+        {
+            name: "name4",
+            price: 4,
+            rating: 5,
+            releaseDate: "12/08/2017",
+            description: "BlaBlaBla"
+        },
+        {
+            name: "name5",
+            price: 5,
+            rating: 5,
+            releaseDate: "12/08/2017",
+            description: "BlaBlaBla"
+        },
+        {
+            name: "name6",
+            price: 6,
+            rating: 5,
+            releaseDate: "12/08/2017",
+            description: "BlaBlaBla"
+        },
+    ]
+
+    return {
+        getBooks: function(){
+            return books;
+        }
+    }
+});
+
+myApp.factory("kartService", function(){
+    var kart = [];
+
+    return {
+        getKart: function(){
+            return kart;
+        },
+        addToKart: function(book){
+            kart.push(book);
+        },
+        buy: function(book){
+            alert("Buy: ", book.name);
+        }
+
+    }
+});
+
+myApp.controller("KartListCtrl", function($scope, kartService){
+    $scope.kart = kartService.getKart();
+
+    $scope.buy = function(book){
+        kartService.buy(book);
+    }
 });
 
 myApp.controller("HeaderCtrl", function($scope) {
@@ -21,55 +101,13 @@ myApp.controller("HeaderCtrl", function($scope) {
     };
 })
 
-myApp.controller("BookListCtrl", function($scope){
-    $scope.books = 
-        [
-            {
-                name: "name1",
-                price: 1,
-                rating: 5,
-                releaseDate: "12/08/2017",
-                description: "BlaBlaBla"
-            },
-            {
-                name: "name2",
-                price: 2,
-                rating: 5,
-                releaseDate: "12/08/2017",
-                description: "BlaBlaBla"
-            },
-            {
-                name: "name3",
-                price: 3,
-                rating: 5,
-                releaseDate: "12/08/2017",
-                description: "BlaBlaBla"
-            },
-            {
-                name: "name4",
-                price: 4,
-                rating: 5,
-                releaseDate: "12/08/2017",
-                description: "BlaBlaBla"
-            },
-            {
-                name: "name5",
-                price: 5,
-                rating: 5,
-                releaseDate: "12/08/2017",
-                description: "BlaBlaBla"
-            },
-            {
-                name: "name6",
-                price: 6,
-                rating: 5,
-                releaseDate: "12/08/2017",
-                description: "BlaBlaBla"
-            },
-        ]
+myApp.controller("BookListCtrl", function($scope, bookService, kartService){
+
+    $scope.books = bookService.getBooks();
 
         $scope.addToKart = function(book){
-            console.log("Add to kart", book);
+            kartService.addToKart(book);
+            
         }
     
 })
